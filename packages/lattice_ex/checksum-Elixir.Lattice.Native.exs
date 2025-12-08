4f82358d16b701220a497e8c01bfc4ca5fa96750
catethos <cloverethos@gmail.com>
2025-12-08 19:36:20 +0800

    Release v0.1.3

--- packages/lattice_ex/checksum-Elixir.Lattice.Native.exs
+++ packages/lattice_ex/checksum-Elixir.Lattice.Native.exs
@@ -1,4 +1,4 @@
 %{
-  "liblattice_nif-v0.1.2-nif-2.17-aarch64-apple-darwin.so" => "sha256:17750d3bd821f58adac529c51cbb8bad5a6e401ad3c7ecade7e9086d93c92a61",
-  "liblattice_nif-v0.1.2-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:c8f7e78a8c68eb8404d67d82e1654e539a9f54843cb7771647d4e8fd4857dc1d"
+  "liblattice_nif-v0.1.3-nif-2.17-aarch64-apple-darwin.so" => "sha256:c02d9038b7d777223bea5867ed30aad18659fa40e615bb18a4de2c6d1d640742",
+  "liblattice_nif-v0.1.3-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:6b1aa2d7446fe3dcbb45dc742eab48535a1511baa027d38f60302e02f264f93f"
 }
